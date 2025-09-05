7d884733e4068884fb88e05558b12b079dd6bb96
Harsh Shishodia <harshshishodia08@gmail.com>
2025-09-05 21:00:04 +0530

    Update WorkerNavbar.tsx

--- src/app/worker/components/WorkerNavbar.tsx
+++ src/app/worker/components/WorkerNavbar.tsx
@@ -130,7 +130,7 @@ export default function WorkerNavbar() {
             {/* Desktop user area */}
             <div className="hidden lg:flex items-center space-x-4">
               <button className="text-black px-3 py-2 font-medium">
-                Aayush Anand
+                Aayush Anand  
               </button>
               <span className="text-gray-500 text-opacity-50 px-3 py-2 text-sm font-medium capitalize">
                 {role}
@@ -183,7 +183,7 @@ export default function WorkerNavbar() {
               >
                 <div className="py-1">
                   <div className="block w-full text-left px-4 py-2 text-sm text:black">
-                    Worker
+                    Aayush Anand  
                     <span className="text-xs text-gray-500 capitalize">
                       {role}
                     </span>
